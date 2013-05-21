b9e70a06f80f48ab2ea516f339695f03dd0570a2
Anthony Smith <anthony@sticksnleaves.com>
2013-05-21 00:44:57 -0400

    Configured rspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+ENV['RAILS_ENV'] = 'test'
+
+require 'shirtsio'
+
+# Require supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+
+end
