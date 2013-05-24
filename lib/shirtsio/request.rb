96ae6e09c9ca84f1955e548406618c6d4981703b
Anthony Smith <anthony@sticksnleaves.com>
2013-05-24 02:07:13 -0400

    Removed raw output.

--- lib/shirtsio/request.rb
+++ lib/shirtsio/request.rb
@@ -1,16 +1,16 @@
 module Shirtsio
   module Request
-    def get(path, options = {}, headers = {}, raw = false)
-      request(:get, path, options, headers, raw)
+    def get(path, options = {}, headers = {})
+      request(:get, path, options, headers)
     end
 
-    def post(path, options = {}, headers = {}, raw = false)
-      request(:post, path, options, headers, raw)
+    def post(path, options = {}, headers = {})
+      request(:post, path, options, headers)
     end
 
     private
     # Perform an HTTP request
-    def request(method, path, options, headers, raw)
+    def request(method, path, options, headers)
       headers.merge!({
         'User-Agent' => user_agent
       })
@@ -26,15 +26,7 @@ module Shirtsio
       end
       Shirtsio::Utils.handle_api_error(response) if response.status != 200
 
-      if raw
-        result = response.body
-      else
-        # The API is returning the "result" encoded as a string rather than
-        # object notation so it requires a second pass of the JSON parser
-        result = Shirtsio::Utils.parse_json(response.body)[:result]
-      end
-
-      result
+      Shirtsio::Utils.parse_json(response.body)[:result]
     end
   end
 end
